50baadf122dd64906cec79fa68d5ac206d02ac8e
JordBousquet <jordbousuqet@yahoo.com.br>
2022-07-22 00:44:50 -0300

    Estilizando a tela de Detalhes

---
+++ pokedex/src/components/Pages/Details/styled.js
@@ -0,0 +1,129 @@
+import styled from "styled-components"
+
+export const DetailsBackground = styled.div`
+    background-color:#DDDDDD;
+    padding:5px;
+`
+
+export const Title = styled.h1`
+position: absolute;
+width: 220px;
+height: 72px;
+left: 40px;
+top: 220px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 48px;
+line-height: 72px;
+
+color: #FFFFFF
+
+`
+
+export const DetailContainer = styled.div`
+position:relative;
+width: 100%;
+height: 650px;
+background: #729F92;
+border-radius: 37.8857px;
+
+`
+
+export const FrontSprite = styled.div`
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 26px;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+background-color:#FFFFFF;
+text-align:center;
+
+`
+
+export const BackSprite = styled.div`
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 355px;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+background-color:#FFFFFF;
+text-align:center;
+`
+
+export const StatusContainer = styled.div`
+position: absolute;
+width: 343px;
+height: 613px;
+left: 360px;
+top: 24px;
+text-align:center;
+
+background: #FFFFFF;
+border-radius: 12px;
+
+`
+
+export const MoveContainer = styled.div`
+position: absolute;
+width: 292px;
+height: 453px;
+left: 771px;
+top: 184px;
+text-align:center;
+
+background: #FFFFFF;
+border-radius: 8px;
+`
+
+export const TypeStyle = styled.li`
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 5px 8px;
+gap: 5px;
+align-items:center;
+justify-content:center;
+
+
+position:relative;
+width: 97px;
+height: 24px;
+left: 774px;
+top: 67px;
+
+
+background: #FF6262;
+border: 1px dashed rgba(255, 255, 255, 0.47);
+border-radius: 8px;
+
+`
+
+export const PokeNameTitle = styled.h1`
+position: absolute;
+width: 238px;
+height: 58px;
+left: 771px;
+bottom:550px;
+
+font-family: 'Inter';
+font-style: normal;
+font-size: 48px;
+line-height: 58px;
+
+
+color: #FFFFFF;
+
+`
+
+export const ImgPoke = styled.img`
+        width:70%;
+        position:relative;
+        top:45px;
+       
+`
